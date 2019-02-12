f90eb9b9f990c89b7bc7e833a5349bfaae582b84
German Carrillo <german.carillo@aurea.com>
2019-02-11 22:36:58 -0500

    Upload sample service

--- src/main/java/com/pichincha/base/Application.java
+++ src/main/java/com/pichincha/base/Application.java
@@ -1,4 +1,28 @@
 package com.pichincha.base;
 
+import com.pichincha.model.Message;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@RestController
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/host/{id}")
+    public Message home(@PathVariable String id) throws UnknownHostException {
+        Message message = new Message();
+        message.setMessage("Hello " + id + " from " + InetAddress.getLocalHost().getHostName());
+        return message;
+    }
+
 }
